ce78733f3e97b82138b4de56d515761e8b95431c
pardeep729 <6729957+pardeep729@users.noreply.github.com>
2022-01-03 10:26:39 +0000

    Setup basic code files

---
+++ main.py
@@ -0,0 +1,18 @@
+# Standard modules
+
+# Installed modules
+import pygame
+
+# Custom modules
+import gobble2
+
+if __name__ == '__main__':
+    pass
+
+"""
+Help:
+https://pythonprojects.io/space-shooter-tutorial
+https://pythonprogramming.altervista.org/using-pygame-sprite-collide_masksprite1-sprite2-for-collision-detection-in-pygame/
+https://www.pygame.org/docs/ref/sprite.html#pygame.sprite.groupcollide
+https://www.pygame.org/docs/ref/sprite.html#pygame.sprite.collide_mask
+"""
